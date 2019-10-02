11a43a666ae3e56c24664bd90c4d44bf9013b93c
kavinshah <38910831+kavinshah@users.noreply.github.com>
2019-10-01 20:13:17 -0400

    make it better

--- swe1app/settings.py
+++ swe1app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g!6+91x@g-q@)o*!ot&25lxe#!#l$0ce!x(ed$kip-!b7q@=4*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
