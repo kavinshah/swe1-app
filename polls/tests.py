14aff936ec1727852111fb824e0c11021ac31a6c
kavinshah <38910831+kavinshah@users.noreply.github.com>
2019-10-15 22:49:50 -0400

    script change

--- polls/tests.py
+++ polls/tests.py
@@ -6,8 +6,7 @@ from .models import Question
 
 
 class QuestionModelTests(TestCase):
-
-    def test_was_published_recently_with_future_question(self):
+	def test_was_published_recently_with_future_question(self):
 		"""
 		was_published_recently() returns False for questions whose pub_date
 		is in the future.
