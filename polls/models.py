65a63b034ab9f2c6d1b400a24fee361a23753665
kavinshah <38910831+kavinshah@users.noreply.github.com>
2019-10-16 09:42:25 -0400

    coverage.py

--- polls/models.py
+++ polls/models.py
@@ -12,7 +12,7 @@ class Question(models.Model):
 
     def was_published_recently(self):
         now = timezone.now()
-        return now >= self.pub_date >= datetime.now() - datetime.timedelta(days=1)
+        return now >= self.pub_date >= now - datetime.timedelta(days=1)
 
     was_published_recently.admin_order_field = "pub_date"
     was_published_recently.boolean = True
