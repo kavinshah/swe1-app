8c78d11f4c0e5b067d8e899d211235b1e41924ce
kavinshah <38910831+kavinshah@users.noreply.github.com>
2019-10-16 06:24:46 -0400

    script change

--- polls/tests.py
+++ polls/tests.py
@@ -14,7 +14,7 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
-        
+
     def test_was_published_recently_with_old_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
@@ -32,7 +32,7 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
-		
+
     def create_question(question_text, days):
     """
     Create a question with the given `question_text` and published the
@@ -41,7 +41,7 @@ class QuestionModelTests(TestCase):
     """
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
-    
+
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
         """
@@ -86,7 +86,7 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question.>']
         )
-        
+
     def test_two_past_questions(self):
         """
         The questions index page may display multiple questions.
@@ -98,7 +98,7 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
-		
+
 class QuestionDetailViewTests(TestCase):
     def test_future_question(self):
         """
@@ -119,4 +119,3 @@ class QuestionDetailViewTests(TestCase):
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
-        
